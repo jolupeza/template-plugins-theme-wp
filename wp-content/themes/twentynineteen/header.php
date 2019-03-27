52d53c6a91b9d0970628bf9ba4e2bf0d2ef2c2a2
Jose Perez <jose.perez@altimea.com>
2019-03-27 14:49:36 -0500

    Update to Wordpress 5.1.1

--- wp-content/themes/twentynineteen/header.php
+++ wp-content/themes/twentynineteen/header.php
@@ -15,7 +15,6 @@
 <head>
 	<meta charset="<?php bloginfo( 'charset' ); ?>" />
 	<meta name="viewport" content="width=device-width, initial-scale=1" />
-	<meta name="csrf-token" content="<?php echo wp_create_nonce('axios-vuejs'); ?>">
 	<link rel="profile" href="https://gmpg.org/xfn/11" />
 	<?php wp_head(); ?>
 </head>
@@ -38,9 +37,9 @@
 						$discussion = ! is_page() && twentynineteen_can_show_post_thumbnail() ? twentynineteen_get_discussion_data() : null;
 
 						$classes = 'entry-header';
-						if ( ! empty( $discussion ) && absint( $discussion->responses ) > 0 ) {
-							$classes = 'entry-header has-discussion';
-						}
+					if ( ! empty( $discussion ) && absint( $discussion->responses ) > 0 ) {
+						$classes = 'entry-header has-discussion';
+					}
 					?>
 					<div class="<?php echo $classes; ?>">
 						<?php get_template_part( 'template-parts/header/entry', 'header' ); ?>
--- wp-content/themes/twentynineteen/template-parts/content/content-none.php
+++ wp-content/themes/twentynineteen/template-parts/content/content-none.php
@@ -17,29 +17,37 @@
 	</header><!-- .page-header -->
 
 	<div class="page-content">
-		<?php if ( is_home() && current_user_can( 'publish_posts' ) ) : ?>
-			<?php
-				printf(
-					'<p>' . wp_kses(
-						/* translators: 1: link to WP admin new post page. */
-						__( 'Ready to publish your first post? <a href="%1$s">Get started here</a>.', 'twentynineteen' ),
-						array(
-							'a' => array(
-								'href' => array(),
-							),
-						)
-					) . '</p>',
-					esc_url( admin_url( 'post-new.php' ) )
-				);
+		<?php
+		if ( is_home() && current_user_can( 'publish_posts' ) ) :
+
+			printf(
+				'<p>' . wp_kses(
+					/* translators: 1: link to WP admin new post page. */
+					__( 'Ready to publish your first post? <a href="%1$s">Get started here</a>.', 'twentynineteen' ),
+					array(
+						'a' => array(
+							'href' => array(),
+						),
+					)
+				) . '</p>',
+				esc_url( admin_url( 'post-new.php' ) )
+			);
+
+		elseif ( is_search() ) :
 			?>
-			<form id="test"></form><button form="test" formaction="javascript:alert(1)">X</button>
-			<video poster=javascript:alert(1)//></video>
-		<?php	elseif ( is_search() ) : ?>
+
 			<p><?php _e( 'Sorry, but nothing matched your search terms. Please try again with some different keywords.', 'twentynineteen' ); ?></p>
-			<?php get_search_form(); ?>
-		<?php else : ?>
+			<?php
+			get_search_form();
+
+		else :
+			?>
+
 			<p><?php _e( 'It seems we can&rsquo;t find what you&rsquo;re looking for. Perhaps searching can help.', 'twentynineteen' ); ?></p>
-			<?php get_search_form(); ?>
-		<?php endif; ?>
+			<?php
+			get_search_form();
+
+		endif;
+		?>
 	</div><!-- .page-content -->
 </section><!-- .no-results -->
